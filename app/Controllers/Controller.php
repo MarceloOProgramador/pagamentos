b05b5e13fc92449aa6e1c75486c8b901d921b555
Marcelo Pereira <marcelooprogramador@gmail.com>
2021-07-05 21:38:07 +0000

    Create project directories and Controller

---
+++ app/Controllers/Controller.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace App\Controllers;
+
+class Controller{
+
+    public function index()
+    {
+
+    }
+
+    public function store(array $datas)
+    {
+
+    }
+
+    public function update(array $datas, int $id)
+    {
+
+    }
+
+    public function delete(int $id)
+    {
+
+    }
+}
---
+++ app/Controllers/userController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\Controller;
+
+class userController extends Controller{
+
+    public function index()
+    {
+
+    }
+
+    public function store(array $datas)
+    {
+
+    }
+
+    public function update(array $datas, int $id)
+    {
+
+    }
+
+    public function delete(int $id)
+    {
+
+    }
+    
+}
